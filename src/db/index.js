96655e812c5c91f6dbb166c01424b4bafff30a1b
Victor Ruiz <vik_ases@live.com.mx>
2019-10-05 14:16:23 -0500

    Lint code

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
-const {URI_DB} = require('../config')
+const { URI_DB } = require('../config')
 
 mongoose.connect(URI_DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 }).then(console.log('Database connect'))
 
 module.exports = mongoose
--- src/models/user.js
+++ src/models/user.js
@@ -1,19 +1,19 @@
-const {Schema, model} = require('mongoose')
+const { Schema, model } = require('mongoose')
 const bcrypt = require('bcrypt')
 
 const userSchema = new Schema({
-    username: String,
-    email:String,
-    password:String
+  username: String,
+  email: String,
+  password: String
 })
 
-userSchema.methods.encryptPassword = async(password) => {
-    const salt = bcrypt.genSalt()
-    return bcrypt.hash(password,salt)
+userSchema.methods.encryptPassword = async (password) => {
+  const salt = bcrypt.genSalt()
+  return bcrypt.hash(password, salt)
 }
 
-userSchema.methods.comparePassword = async ()=>{
-    return bcrypt.compare(password, this.password)
+userSchema.methods.comparePassword = async (password) => {
+  return bcrypt.compare(password, this.password)
 }
 
-module.exports = model('User',userSchema)
+module.exports = model('User', userSchema)
