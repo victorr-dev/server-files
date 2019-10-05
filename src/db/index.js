c7a855583bcefd30278e364c8a325bfab897b041
Victor Ruiz <vik_ases@live.com.mx>
2019-10-04 20:37:48 -0500

    Config connection to database

---
+++ src/db/index.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const {URI_DB} = require('../config')
+
+mongoose.connect(URI_DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(console.log('Database connect'))
+
+module.exports = mongoose
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const { PORT } = require('./config')
 
 async function main () {
   await app.listen(PORT)
+  await require('./db')
   console.log(`Server is running in port ${PORT}`)
 }
 
