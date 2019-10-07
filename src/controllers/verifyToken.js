9b06449c9f64d959398c04a84215b6cd192a00f9
Victor Ruiz <vik_ases@live.com.m.x>
2019-10-07 15:43:24 -0500

    Lint code

--- src/controllers/verifyToken.js
+++ src/controllers/verifyToken.js
@@ -1,15 +1,15 @@
-const jwt = require('jsonwebtoken');
-const config = require('../config');
+const jwt = require('jsonwebtoken')
+const config = require('../config')
 
-async function verifyToken(req, res, next) {
-    const token = req.headers['x-access-token'];
-    if (!token) {
-        return res.status(401).send({ auth: false, message: 'No token provided' });
-    }
-    // Decode the Tokenreq.userId = decoded.id;
-    const decoded = await jwt.verify(token, config.SECRET);
-    req.userId = decoded.id;
-    next();
+async function verifyToken (req, res, next) {
+  const token = req.headers['x-access-token']
+  if (!token) {
+    return res.status(401).send({ auth: false, message: 'No token provided' })
+  }
+  // Decode the Tokenreq.userId = decoded.id;
+  const decoded = await jwt.verify(token, config.SECRET)
+  req.userId = decoded.id
+  next()
 }
 
-module.exports = verifyToken;
+module.exports = verifyToken
