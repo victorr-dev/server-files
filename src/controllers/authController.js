8ec21c2ce0fb1710b0f1bbf3e8f1a70841d17d9c
Victor Ruiz <vik_ases@live.com.m.x>
2019-10-07 15:44:13 -0500

    Implement singup and logout

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,13 +1,21 @@
-const { Router } = require('express')
+const {
+  Router
+} = require('express')
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
-const {SECRET} = require('../config')
+const {
+  SECRET
+} = require('../config')
 const verifyToken = require('./verifyToken')
 
 const api = Router()
 
 api.post('/singin', async (req, res, next) => {
-  const {username, email, password} = req.body
+  const {
+    username,
+    email,
+    password
+  } = req.body
   const user = new User({
     username,
     email,
@@ -18,27 +26,58 @@ api.post('/singin', async (req, res, next) => {
 
   await user.save()
 
-  const token = jwt.sign({ id: user.id }, SECRET, {
+  const token = jwt.sign({
+    id: user.id
+  }, SECRET, {
     expiresIn: 60 * 60 * 24 // expires in 24 hours
-  });
+  })
 
-  res.status(200).json({auth: true, token})
+  res.status(200).json({
+    auth: true,
+    token
+  })
 })
 
 api.get('/me', verifyToken, async (req, res, next) => {
-  const user = await User.findById(req.userId, { password: 0});
-    if (!user) {
-        return res.status(404).send("No user found.");
-    }
-  res.status(200).json(user);
+  const user = await User.findById(req.userId, {
+    password: 0
+  })
+  if (!user) {
+    return res.status(404).send('No user found.')
+  }
+  res.status(200).json(user)
 })
 
-api.get('/singup', (req, res, next) => {
-  res.json('singup')
+api.post('/singup', async (req, res, next) => {
+  const user = await User.findOne({
+    email: req.body.email
+  })
+  if (!user) {
+    return res.status(404).send('No user found')
+  }
+  const validPassword = await user.comparePassword(req.body.password, user.password)
+
+  if (!validPassword) {
+    return res.status(404).send({
+      auth: false,
+      token: null
+    })
+  }
+
+  const token = jwt.sign({
+    id: user.id
+  }, SECRET, {
+    expiresIn: 60 * 60 * 24 // expires in 24 hours
+  })
+
+  res.status(200).json({
+    auth: true,
+    token
+  })
 })
 
 api.get('/logout', (req, res, next) => {
-  res.json('logout')
+  res.status(200).send({ auth: false, token: null })
 })
 
 module.exports = api
